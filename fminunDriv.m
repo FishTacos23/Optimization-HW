92035f32b4454f737ff8ebeeb72a723490dbafde
FishTacos23 <32417793+FishTacos23@users.noreply.github.com>
2018-02-06 20:29:38 -0700

    3 var active

--- fminunDriv.m
+++ fminunDriv.m
@@ -7,20 +7,20 @@ function [] = fminunDriv()
     ngrad = 0.; % counter for gradient evaluations
     
     % starting point, set to be column vector
-    %x0 = [10.; 10.; 10.]; 
-    x0 = [-1.5;1];
+    x0 = [10.; 10.; 10.]; 
+    %x0 = [-1.5;1];
     
     algoflag = 1; % 1=steepest descent; 2=conjugate gradient; 3=BFGS quasi-Newton
     stoptol = 1.e-5; % stopping tolerance, all gradient elements must be < stoptol
     alpha = 0.1;
 
     % ---------- create contour ------------
-    %plot3D(-11, 11, -11, 11, -8.5556);
-    plot2D(-2, 2, -2, 2);
+    plot3D(-11, 11, -11, 11, -8.5556);
+    %plot2D(-2, 2, -2, 2);
     num_plot = 20;
     
     % ---------- call fminun----------------
-    [xopt, fopt, exitflag] = fminun(@obj2var, @gradobj2var, x0, stoptol, algoflag, alpha, num_plot);
+    [xopt, fopt, exitflag] = fminun(@obj3var, @gradobj3var, x0, stoptol, algoflag, alpha, num_plot);
    
     xopt
     fopt
