c43bc15383541480447d3db66348d95d512c5576
FishTacos23 <32417793+FishTacos23@users.noreply.github.com>
2018-02-07 20:09:28 -0700

    conjugat gradient working

--- conjugant_gradient.m
+++ conjugant_gradient.m
@@ -1,17 +1,17 @@
-function [s, a_star, mag] = conjugant_gradient(f, grad, x, alpha, obj, plot_bool)
+function [x, f, grad, s] = conjugant_gradient(f, grad, x, alpha, obj, gradobj, plot_bool, s)
+    
+    mag = sqrt(s(:,end)'*s(:,end)); % magnitude
 
-    s = -grad;   % search direction
-    mag = sqrt(grad'*grad); % magnitude
-
-    f_a(1) = f;
+    f_a(1) = f(:,end);
 
     % FIRST STEP
     a(2) = alpha;
-    x_n = x + a(2)*s/mag;
+    x_n = x(:,end) + a(2)*s(:,end)/mag;
+%     x_n = x(:,end) + a(2)*s;
     f_a(2) = obj(x_n);
 
     if plot_bool
-        plot(x_n(1), x_n(2), '--r*');
+        plot(x_n(1), x_n(2), 'r.', 'LineWidth', 5);
     end
 
     % Iterator Variables
@@ -39,19 +39,25 @@ function [s, a_star, mag] = conjugant_gradient(f, grad, x, alpha, obj, plot_bool
         end
 
         % set point
-        x_n = x + a(i)*s/mag;
+        x_n = x(:,end) + (a(i)*s(:,end)/mag);
         f_a(i) = obj(x_n);
 
         % Iteration funcs
         if plot_bool
-            plot(x_n(1), x_n(2), 'r*');
+            plot(x_n(1), x_n(2), 'r.', 'LineWidth', 5);
         end
 
         i = i + 1;
 
     end
 
-    % Set values to calc a_star       
     a_star = parabolic_line_search(a, f_a);
+
+    x(:,end+1) = x(:,end) + a_star*s(:,end)/mag;
+    f(:,end+1) = obj(x(:,end));
+    grad(:,end+1) = gradobj(x(:,end));
+    
+    Beta = grad(:,end)'*grad(:,end)/(grad(:,end-1)'*grad(:,end-1));
+    s(:,end+1) = -grad(:,end) + Beta*s(:,end);
 
  end
